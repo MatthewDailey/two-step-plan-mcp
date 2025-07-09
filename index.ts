2e7604bc92f6dbf99f82198d7725960ca015be5b
Matt Dailey <matthew.j.dailey@gmail.com>
2025-07-09 16:38:50 -0700

    bump version

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import { generateTwoStepPlan } from "./generateTwoStepPlan.js";
 const server = new Server(
   {
     name: "two-step-plan",
-    version: "0.2.3",
+    version: "0.3.0",
   },
   {
     capabilities: {
