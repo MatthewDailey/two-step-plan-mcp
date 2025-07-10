f357450f3b7862cccd598f9bf32e48daddf41806
Matt Dailey <matthew.j.dailey@gmail.com>
2025-07-10 08:41:19 -0700

    remove testExec.ts

--- testExec.ts
+++
@@ -1,8 +0,0 @@
-import { execClaudePrompt } from "./generateTwoStepPlan.js";
-
-async function test() {
-  const result = await execClaudePrompt(`Write a helloworld.ts file`, "Failed to test", console.log);
-  console.log("Result:", result);
-}
-
-test().catch(console.error);
